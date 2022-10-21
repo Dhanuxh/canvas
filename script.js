0b80d88d6fcd0313a80ee8cf41a83df21d6ed8ce
Dhanuxh <113832882+Dhanuxh@users.noreply.github.com>
2022-10-21 17:13:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+/* Iteration 4: Make the Everything Work */
+// <!-- Michael Jackson - background, socks,shirt,pant,shoes,gloves,jacket,face,hat -->
+const background =document.getElementById("background")
+const butterbackground =document.getElementById("butterbackground")
+
+butterbackground.oninput=()=>{
+  background.style.fill=butterbackground.value;
+}
+const socks =document.getElementById("socks")
+const buttersocks =document.getElementById("buttersocks")
+
+buttersocks.oninput=()=>{
+  socks.style.fill=buttersocks.value;
+}
+const shirt =document.getElementById("shirt")
+const buttershirt =document.getElementById("buttershirt")
+
+buttershirt.oninput=()=>{
+  shirt.style.fill=buttershirt.value;
+}
+const pant =document.getElementById("pant")
+const butterpant =document.getElementById("butterpant")
+
+butterpant.oninput=()=>{
+  pant.style.fill=butterpant.value;
+}
+ const shoes =document.getElementById("shoes")
+ const buttershoes =document.getElementById("buttershoes")
+
+ buttershoes.oninput=()=>{
+  shoes.style.fill=buttershoes.value;
+ }
+const gloves =document.getElementById("gloves")
+const buttergloves =document.getElementById("buttergloves")
+
+buttergloves.oninput=()=>{
+  gloves.style.fill=buttergloves.value;
+}
+const jacket =document.getElementById("jacket")
+const butterjacket =document.getElementById("butterjacket")
+
+butterjacket.oninput=()=>{
+  jacket.style.fill=butterjacket.value;
+}
+const face =document.getElementById("face")
+const butterface =document.getElementById("butterface")
+
+butterface.oninput=()=>{
+  face.style.fill=butterface.value;
+}
+
+const hat =document.getElementById("hat")
+const butterhat =document.getElementById("butterhat")
+
+butterhat.oninput=()=>{
+  hat.style.fill=butterhat.value;
+}
+const hair =document.getElementById("hair")
+const butterhair =document.getElementById("butterhair")
+
+butterhat.oninput=()=>{
+  hair.style.fill=butterhat.value;
+}
